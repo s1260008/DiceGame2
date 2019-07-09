2d8a2c6ba9e1c3234578f6e2415458c3a56b4e05
s1260008 <s1260008@call1dc5.u-aizu.ac.jp>
2019-07-09 18:26:33 +0900

    Changed the number of dice to 3

--- DiceGame.c
+++ DiceGame.c
@@ -3,7 +3,7 @@
 #include<time.h>
 
 int main(){
-  int d1,d2,sum;
+  int d1,d2,d3,sum;
   char name[30];
 
   printf("What is our name?\n");
@@ -14,10 +14,11 @@ int main(){
   srand(time(NULL));
   d1=rand()%6+1;
   d2=rand()%6+1;
-  sum=d1+d2;
-  printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n\n",d1,d2,sum);
+  d3=rand()%6+1;
+  sum=d1+d2+d3;
+  printf("Die 1: %d\nDie 2: %d\nDie 3: %d\nTotal value: %d\n\n",d1,d2,d3,sum);
 
-  if(sum>7)printf("%s won!\n",name);
+  if(sum>9)printf("%s won!\n",name);
   else printf("%s lost!\n",name);
   
   return 0;
